709d14395426e07269530f725bb8f41079db7f0a
teddychuks <chukwukaohuruogu@gmail.com>
2023-12-27 09:13:09 +0100

    main

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,7 +51,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 
     const url = `${req.protocol}://${req.get("host")}user/myaccount`;
 
-    await new Email(newUser, url).sendWelcome();
+    await new Email(newUser, url);
 
     createSendToken(newUser, 201, res);
   } catch (error) {
